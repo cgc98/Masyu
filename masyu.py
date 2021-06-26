600d05630b8bd1cd69d6acfa0167d55b6dfa97bf
Cameron Camacho <cgc98@cornell.edu>
2021-06-26 18:24:16 -0400

    Can print board

--- masyu.py
+++ masyu.py
@@ -6,8 +6,13 @@ class Tile:
         self.down_status = 0
         self.left_status = 0
         self.solved = False
-    def print(self):
-        print(self.color)
+ 
+def convert_status_to_char(status):
+    if status == 0:
+        return "?"
+    elif status == -1:
+        return "N"
+    return "Y"
 
 class Board:
     def __init__(self, colors):
@@ -16,9 +21,25 @@ class Board:
             for j in range(len(colors[i])):
                 self.board[i][j] = Tile(colors[i][j])
     def print(self):
-        for i in range(len(self.board)):
-            for j in range(len(self.board[i])):
-                self.board[i][j].print()
+        for i in range((len(self.board))-1):
+            string = ""
+            for j in range((len(self.board[i]))-1):
+                string += self.board[i][j].color + " "
+                string += convert_status_to_char(self.board[i][j].right_status) + " "
+            string += self.board[i][len(self.board)-1].color
+            print(string)
+            string_two = ""
+            for j in range((len(self.board[i]))-1):
+                string_two += convert_status_to_char(self.board[i][j].down_status) + "   "
+            string_two += convert_status_to_char(self.board[i][len(self.board)-1].down_status)
+            print(string_two)
+        string = ""
+        for j in range((len(self.board[(len(self.board))-1]))-1):
+            string += self.board[(len(self.board))-1][j].color + " "
+            string += convert_status_to_char(self.board[(len(self.board))-1][j].right_status) + " "
+        string += self.board[(len(self.board))-1][len(self.board)-1].color
+        print(string)
+        
 
 colors = [['E','E','E','E','E','E','E','E','E','E',],\
 ['W','E','W','E','E','W','B','E','B','W',],\
@@ -32,5 +53,6 @@ colors = [['E','E','E','E','E','E','E','E','E','E',],\
 ['E','E','E','E','E','B','E','B','W','B',]]
 
 board = Board(colors)
+board.print()
 
 
