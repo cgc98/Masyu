94c6d63caa5ea21511d7bd53c551c5cadd57670e
Cameron Camacho <cgc98@cornell.edu>
2021-06-27 02:51:28 -0400

    comments

--- masyu.py
+++ masyu.py
@@ -82,8 +82,10 @@ class Board:
 
 
 def solve_empty_tile(board, tile):
+    #If already solved, set to solved
     if tile.get_number_n() + tile.get_number_y() == 4:
         tile.solved = True
+    #If 3 are blocked, all statuses are -1
     elif tile.get_number_n() >= 3:
         if tile.up_status == 0:
             tile.up_status = -1
@@ -94,6 +96,7 @@ def solve_empty_tile(board, tile):
         elif tile.left_status == 0:
             tile.left_status = -1
         tile.solved = True
+    #If two are yes, the rest are no
     elif tile.get_number_y() == 2:
         if tile.up_status == 0:
             tile.up_status = -1
@@ -104,6 +107,7 @@ def solve_empty_tile(board, tile):
         if tile.left_status == 0:
             tile.left_status = -1
         tile.solved = True
+    #If two are no and one is yes, the other is yes
     elif (tile.get_number_n() == 2) and (tile.get_number_y() == 1):
         if tile.up_status == 0:
             tile.up_status = 1
@@ -118,6 +122,7 @@ def solve_empty_tile(board, tile):
 
 def solve_white_tile(board, tile):
     if (tile.get_number_y() + tile.get_number_n() > 0) and (tile.solved == False):
+        #One status tells all status, so checks for any nonzero statuses
         if tile.up_status != 0:
             tile.down_status = tile.up_status
             tile.left_status = -1 * tile.up_status
@@ -138,6 +143,7 @@ def solve_white_tile(board, tile):
             tile.down_status = -1 * tile.left_status
             tile.up_status = -1 * tile.left_status
             tile.solved = True
+    #Ensures that one bend is made for solved tiles (if one side is straight, the other cannot be)
     if tile.solved == True:
         if tile.right_status == 1:
             right_tile = board.board[tile.x][tile.y + 1]
@@ -155,6 +161,7 @@ def solve_white_tile(board, tile):
                 board.board[tile.x - 1][tile.y].up_status = -1
 
 def distribute_data(board, tile):
+    #Make the status of surrounding tiles match any updates
     if (tile.up_status != 0) and (tile.x != 0):
         up_tile = board.board[tile.x - 1][tile.y]
         if up_tile.down_status == 0:
@@ -173,6 +180,7 @@ def distribute_data(board, tile):
             board.board[tile.x][tile.y - 1].right_status = tile.left_status
 
 def solve_black_tile(board, tile):
+    #Checks if a two tile extension is allowed, and if not blocks direction
     if tile.up_status == 0:
         up_tile = board.board[tile.x - 1][tile.y]
         if (up_tile.up_status == -1) or (up_tile.left_status == 1) or (up_tile.right_status == 1):
@@ -189,6 +197,7 @@ def solve_black_tile(board, tile):
         left_tile = board.board[tile.x][tile.y - 1]
         if (left_tile.left_status == -1) or (left_tile.up_status == 1) or (left_tile.down_status == 1):
             tile.left_status = -1
+    #Makes opposite sides opposite status
     if tile.up_status != 0:
         tile.down_status = -1 * tile.up_status
     if tile.down_status != 0:
@@ -197,6 +206,7 @@ def solve_black_tile(board, tile):
         tile.right_status = -1 * tile.left_status
     if tile.right_status != 0:
         tile.left_status = -1 * tile.right_status
+    #Extends any branches another tile and distributes next tile's data
     if tile.up_status == 1:
         board.board[tile.x - 1][tile.y].up_status = 1
         distribute_data(board, board.board[tile.x - 1][tile.y])
@@ -209,10 +219,97 @@ def solve_black_tile(board, tile):
     if tile.right_status == 1:
         board.board[tile.x][tile.y + 1].right_status = 1
         distribute_data(board, board.board[tile.x][tile.y + 1])
+    #If two branches exist, solve
     if tile.get_number_y == 2:
         tile.solved = True
 
+def get_endpoint(board, tile, dir):
+    #Recursive function that continues down line until it reaches other endpoint
+    if dir == "Up":
+        if board.board[tile.x - 1][tile.y].get_number_y() == 1:
+            return board.board[tile.x - 1][tile.y]
+        else:
+            if board.board[tile.x - 1][tile.y].up_status == 1:
+                return get_endpoint(board, board.board[tile.x - 1][tile.y], "Up")
+            elif board.board[tile.x - 1][tile.y].right_status == 1:
+                return get_endpoint(board, board.board[tile.x - 1][tile.y], "Right")
+            elif board.board[tile.x - 1][tile.y].left_status == 1:
+                return get_endpoint(board, board.board[tile.x - 1][tile.y], "Left")
+    elif dir == "Right":
+        if board.board[tile.x][tile.y + 1].get_number_y() == 1:
+            return board.board[tile.x][tile.y + 1]
+        else:
+            if board.board[tile.x][tile.y + 1].up_status == 1:
+                return get_endpoint(board, board.board[tile.x][tile.y + 1], "Up")
+            elif board.board[tile.x][tile.y + 1].down_status == 1:
+                return get_endpoint(board, board.board[tile.x][tile.y + 1], "Down")
+            elif board.board[tile.x][tile.y + 1].right_status == 1:
+                return get_endpoint(board, board.board[tile.x][tile.y + 1], "Right")
+    elif dir == "Down":
+        if board.board[tile.x + 1][tile.y].get_number_y() == 1:
+            return board.board[tile.x + 1][tile.y]
+        else:
+            if board.board[tile.x + 1][tile.y].down_status == 1:
+                return get_endpoint(board, board.board[tile.x + 1][tile.y], "Down")
+            elif board.board[tile.x + 1][tile.y].right_status == 1:
+                return get_endpoint(board, board.board[tile.x + 1][tile.y], "Right")
+            elif board.board[tile.x + 1][tile.y].left_status == 1:
+                return get_endpoint(board, board.board[tile.x + 1][tile.y], "Left")
+    elif dir == "Left":
+        if board.board[tile.x][tile.y - 1].get_number_y() == 1:
+            return board.board[tile.x][tile.y - 1]
+        else:
+            if board.board[tile.x][tile.y - 1].up_status == 1:
+                return get_endpoint(board, board.board[tile.x][tile.y - 1], "Up")
+            elif board.board[tile.x][tile.y - 1].down_status == 1:
+                return get_endpoint(board, board.board[tile.x][tile.y - 1], "Down")
+            elif board.board[tile.x][tile.y - 1].left_status == 1:
+                return get_endpoint(board, board.board[tile.x][tile.y - 1], "Left")
+
 def solve_tile(board, tile):
+    #Endpoint tracking
+    if (tile.solved == False) and (tile.get_number_y() == 1):
+        if tile.up_status == 1:
+            end = get_endpoint(board, tile, "Up")
+            if end.x == tile.x:
+                if end.y - tile.y == 1:
+                    tile.right_status = -1
+                elif tile.y - end.y == 1:
+                    tile.left_status = -1
+            elif end.y == tile.y:
+                if end.x - tile.x == 1:
+                    tile.down_status = -1
+        elif tile.right_status == 1:
+            end = get_endpoint(board, tile, "Right")
+            if end.y == tile.y:
+                if end.x - tile.x == 1:
+                    tile.down_status = -1
+                elif tile.x - end.x == 1:
+                    tile.up_status = -1
+            elif end.x == tile.x:
+                if tile.y - end.y == 1:
+                    tile.left_status = -1
+        elif tile.down_status == 1:
+            end = get_endpoint(board, tile, "Down")
+            if end.x == tile.x:
+                if end.y - tile.y == 1:
+                    tile.right_status = -1
+                elif tile.y - end.y == 1:
+                    tile.left_status = -1
+            elif end.y == tile.y:
+                if tile.x - end.x == 1:
+                    tile.up_status = -1
+        else:
+            end = get_endpoint(board, tile, "Left")
+            if end.y == tile.y:
+                if end.x - tile.x == 1:
+                    tile.down_status = -1
+                elif tile.x - end.x == 1:
+                    tile.up_status = -1
+            elif end.x == tile.x:
+                if end.y - tile.y == 1:
+                    tile.right_status = -1
+    #Solve by color
     if tile.color == 'E':
         if tile.solved == False:
             solve_empty_tile(board, tile)
@@ -224,6 +321,7 @@ def solve_tile(board, tile):
     distribute_data(board, tile)
 
 def solve(board):
+    #Solve all tiles
     for i in range(len(board.board)):
         for j in range(len(board.board[i])):
             solve_tile(board, board.board[i][j])
